b19ba40e4614f50adf7e59a0e09cd23cd5158407
jlowry <jlowry@708fe36c-07b9-ea95-be32-11f1da657e33>
2014-03-17 06:51:15 +0000

    Update endpoints

--- trunk/app/attendance_api.py
+++ trunk/app/attendance_api.py
@@ -1,11 +1,18 @@
 """Attendance API implemented using Google Cloud Endpoints."""
 
+import datetime
 import endpoints
+import logging
+
+from google.appengine.api import namespace_manager
+from google.appengine.ext import ndb
+
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
-from google.appengine.ext import ndb
+from pytz import timezone
+from pytz import utc
 
 from models import Attendance
 from models import StudentPresent
@@ -31,6 +38,28 @@ class AttendanceRecord(messages.Message):
 class AttendanceAPI(remote.Service):
   """Attendance API v1."""
 
+  @staticmethod
+  def today_as_ordinal(class_timezone):
+    naive_time = datetime.datetime.now()
+    utc_time = utc.localize(naive_time)
+    tz = timezone(class_timezone)
+    local_time = utc_time.astimezone(tz)
+    return local_time.date().toordinal()
+
+  @staticmethod
+  def is_present(attendance, student_key):
+    for student_present in attendance.attending:
+      if student_present.student == student_key:
+        return True
+    return False
+
+  @staticmethod
+  def is_enrolled(classname, student_key):
+    for student_enrolled in classname.enrolled:
+      if student_enrolled == student_key:
+        return True
+    return False
+
   @endpoints.method(AttendanceRecord, message_types.VoidMessage, path='post')
   def post_attendance(self, request):
     """Register whether or not a single student attended class.
@@ -42,26 +71,49 @@ class AttendanceAPI(remote.Service):
       An instance of message_types.VoidMessage.
     """
     current_user = endpoints.get_current_user()
+    if current_user is None:
+      raise endpoints.UnauthorizedException('Invalid token.')
+    domain = current_user.email().split('@')[1]
+    if domain == 'gmail.com': domain = ''
+    namespace_manager.set_namespace(domain)
+
     student_key = ndb.Key('Student', request.student)
+    student = student_key.get()
+    if not student:
+      raise endpoints.BadRequestException('No student: %s' % request.student)
     classname_key = ndb.Key('Class', request.classname)
     classname = classname_key.get()
-    # can only edit attendance for today
-    if classname.date_is_today(request.date):
-      attendance_key = ndb.Key('Class', request.classname, 
-                               'Attendance', request.date)
-      attendance = attendance_key.get()
-      if not attendance:
-        attendance = Attendance(key=attendance_key, attending=[])
-      if request.attended:
-        if not attendance.is_present(student_key):
-          attendance.attending.append(StudentPresent(student=student_key))
-          attendance.put()
-      else:
-        try:
-          attendance.attending.remove(student_present)
-          attendance.put()
-        except ValueError:
-          pass
+    if not classname:
+      raise endpoints.BadRequestException('No class: %s' % request.classname)
+
+    if not AttendanceAPI.is_enrolled(classname, student_key):
+      raise endpoints.BadRequestException(
+        'Student %s is not enrolled in class %s' % (student_key, classname.name))
+
+    today = AttendanceAPI.today_as_ordinal(classname.timezone)
+    if request.date != today:
+      raise endpoints.BadRequestException('Date must be %s, but is: %s' 
+                                          % (today, request.date))
+
+    logging.info('Updating class: %s student: %s date: %s user: %s attend: %s' % 
+                 (classname.name, request.student, request.date, 
+                  current_user.email(), request.attended))
+
+    attendance_key = ndb.Key('Class', request.classname, 
+                             'Attendance', request.date)
+    attendance = attendance_key.get()
+    if not attendance:
+      attendance = Attendance(key=attendance_key, attending=[])
+    if request.attended:
+      if not AttendanceAPI.is_present(attendance, student_key):
+        attendance.attending.append(StudentPresent(student=student_key))
+        attendance.put()
+    else:
+      try:
+        attendance.attending.remove(student_present)
+        attendance.put()
+      except ValueError:
+        pass
     return message_types.VoidMessage()
 
 
--- trunk/app/models.py
+++ trunk/app/models.py
@@ -1,7 +1,3 @@
-import datetime
-
-from pytz import timezone
-from pytz import utc
 
 from google.appengine.ext import ndb
 
@@ -17,13 +13,6 @@ class Class(ndb.Model):
   default_hours = ndb.FloatProperty(default=None)
   timezone = ndb.StringProperty(default='US/Pacific')
 
-  def date_is_today(self, date):
-    naive_time = datetime.datetime.now()
-    utc_time = utc.localize(naive_time)
-    tz = timezone(self.timezone)
-    local_time = utc_time.astimezone(tz)
-    return local_time.date().toordinal() == date
-
 
 class StudentPresent(ndb.Model):
   student = ndb.KeyProperty(required=True)
@@ -36,9 +25,3 @@ class Attendance(ndb.Model):
   # attendance_key = ndb.Key('Class', int(class_id), 'Attendance', int(date_ordinal))
   attending = ndb.StructuredProperty(StudentPresent, repeated=True)
 
-  @classmethod
-  def is_present(self, student_key):
-    for student_present in attending:
-      if student_present.student == student_key:
-        return True
-    return False
